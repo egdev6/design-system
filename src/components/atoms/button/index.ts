3cf61c2db842703b2d8d20442880d929570a1d88
Quique <enrique.gomez@scrm.lidl>
2025-05-07 21:03:33 +0200

    Initial commit

---
+++ src/components/atoms/button/index.ts
@@ -0,0 +1,3 @@
+import Button from './Button';
+export * from './types'
+export default Button;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@atoms': path.resolve(__dirname, './src/components/atoms'),
+      '@molecules': path.resolve(__dirname, './src/components/molecules'),
+      '@organisms': path.resolve(__dirname, './src/components/organisms'),
+      '@templates': path.resolve(__dirname, './src/components/templates'),
+      '@styles': path.resolve(__dirname, './src/styles')
+    }
+  }
+});
