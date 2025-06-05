3ca757180d89b0b6027a8c91adbddbaaeb7e409f
Enrique Gómez <egdev6@gmail.com>
2025-06-05 21:49:34 +0200

    [FIX] Header as prop

--- src/components/atoms/header/Header.tsx
+++ src/components/atoms/header/Header.tsx
@@ -9,32 +9,36 @@ const Header = ({
   children,
   srOnly = false,
   id,
-  as,
+  fontSize,
   ...rest
 }: HeaderProps) => {
   const Component = tag;
 
-  const fontByTag = () => {
-    switch (as) {
+  const fontByTag = (fs: string) => {
+    switch (fs) {
       case 'h1':
-        return '!fs-h1 tablet:!fs-tablet-h1';
+        return 'fs-h1 tablet:fs-tablet-h1';
       case 'h2':
-        return '!fs-h2 tablet:!fs-tablet-h2';
+        return 'fs-h2 tablet:fs-tablet-h2';
       case 'h3':
-        return '!fs-h3 tablet:!fs-tablet-h3';
+        return 'fs-h3 tablet:fs-tablet-h3';
       case 'h4':
-        return '!fs-h4 tablet:!fs-tablet-h4';
+        return 'fs-h4 tablet:fs-tablet-h4';
       case 'h5':
-        return '!fs-h5 tablet:!fs-tablet-h5';
+        return 'fs-h5 tablet:fs-tablet-h5';
       case 'h6':
-        return '!fs-h6 tablet:!fs-tablet-h6';
+        return 'fs-h6 tablet:fs-tablet-h6';
       default:
         return '';
     }
   };
 
   const props = {
-    className: cn(headerVariants({ font, tag, prominent, srOnly }), fontByTag(), className),
+    className: cn(
+      headerVariants({ font, prominent, srOnly }),
+      fontSize ? fontByTag(fontSize) : cn(headerVariants({ tag })),
+      className
+    ),
     id: id || undefined,
     ...rest
   };
